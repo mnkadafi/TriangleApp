d91ef1ff7b90a206f399d79c040ca1a17cb89db1
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2020-12-03 17:34:11 +0700

    Initial Commit

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by Mochamad Nurkhayal Kadafi on 03/12/20.
+//  Copyright © 2020 Mochamad Nurkhayal Kadafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
