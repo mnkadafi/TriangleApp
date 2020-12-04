e565d45b05a6f1a132b5a87171445c1ce97e1d38
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2020-12-04 10:01:26 +0700

    Add Pythagorean Triangle Detection Feature

--- TriangleAppTests/TriangleAppTests.swift
+++ TriangleAppTests/TriangleAppTests.swift
@@ -10,25 +10,72 @@ import XCTest
 @testable import TriangleApp
 
 class TriangleAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  func testInvalidInputSlides() {
+    XCTAssertThrowsError(try detectTriangle(-1, 2, 3)) { error in
+      XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testDetectEquilateralTriangle() {
+    XCTAssertEqual(try detectTriangle(2, 2, 2), "Segitiga Sama Sisi")
+  }
+  
+  func testDetectIsoscelesTriangle() {
+    XCTAssertEqual(try detectTriangle(8, 8, 10), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(8, 10, 8), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(8, 10, 10), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(10, 8, 10), "Segitiga Sama Kaki")
+  }
+  
+  func testDetectRandomTriangle() {
+    XCTAssertEqual(try detectTriangle(3, 5, 7), "Segitiga Sembarang")
+  }
+  
+  func testInequalityTriangle() {
+    XCTAssertThrowsError(try detectTriangle(4, 1, 2)) { error in
+      XCTAssertEqual(error as? TriangleError, TriangleError.inequalityInput)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCTAssertThrowsError(try detectTriangle(5, 1, 3)) { error in
+      XCTAssertEqual(error as? TriangleError, TriangleError.inequalityInput)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+  }
+  
+  func testDetectPhytagorasTriangle() {
+    XCTAssertEqual(try detectTriangle(6, 8, 10), "Segitiga Siku-Siku")
+  }
+  
+  func detectTriangle(
+    _ sideA: Int,
+    _ sideB: Int,
+    _ sideC: Int
+  ) throws -> String {
+    let sides = [sideA, sideB, sideC].sorted()
+    
+    for side in sides {
+      if side < 0 {
+        throw TriangleError.invalidInput
+      }
     }
+    
+    if sides[1] + sides[0] <= sides[2] {
+      throw TriangleError.inequalityInput
+    } else if sides[0] == sides[1] && sides[0] == sides[2] {
+      return "Segitiga Sama Sisi"
+    } else if sides[0] == sides[1] || sides[1] == sides[2] {
+      return "Segitiga Sama Kaki"
+    } else if Double((sides[0] * sides[0] + sides[1] * sides[1])).squareRoot() == Double(sides[2]) {
+      return "Segitiga Siku-Siku"
+    } else {
+      return "Segitiga Sembarang"
+    }
+    
+  }
 
+  enum TriangleError: Error {
+    case invalidInput
+    case inequalityInput
+  }
+  
 }
